b5837c28f77f9fa7b319a9fb314806194d53772b
Lucas Peres <lucasperes95@gmail.com>
2022-07-23 03:59:03 -0300

    DNA: renaming 'buffer', 'dna', and 'bases'

--- dna/CountingDNANucleotides.c
+++ dna/CountingDNANucleotides.c
@@ -7,30 +7,30 @@
 #define G 2
 #define T 3
 
-void count_bases(int *bases, const char *dna)
+void count_bases(int *buffer, const char *dna)
 {
     for (int i = 0; i < strlen(dna); i++)
         if (dna[i] == 'A')
-            bases[A]++;
+            buffer[A]++;
         else if (dna[i] == 'C')
-            bases[C]++;
+            buffer[C]++;
         else if (dna[i] == 'G')
-            bases[G]++;
+            buffer[G]++;
         else if (dna[i] == 'T')
-            bases[T]++;
+            buffer[T]++;
 }
 
 int main(int argc, char **argv)
 {
     size_t size = 0; // ignore
-    char *buffer = NULL;
-    getline(&buffer, &size, stdin);
+    char *dna = NULL;
+    getline(&dna, &size, stdin);
 
-    buffer[strcspn(buffer, "\n")] = 0; // replace newline (if exists) by 0
+    dna[strcspn(dna, "\n")] = 0; // replace newline (if exists) by 0
 
-    int bases[4] = {0, 0, 0, 0};
-    count_bases(bases, buffer);
-    printf("A: %d\nC: %d\nG: %d\nT: %d\n", bases[A], bases[C], bases[G], bases[T]);
+    int counter[4] = {0, 0, 0, 0};
+    count_bases(counter, dna);
+    printf("A: %d\nC: %d\nG: %d\nT: %d\n", counter[A], counter[C], counter[G], counter[T]);
 
-    free(buffer);
+    free(dna);
 }
